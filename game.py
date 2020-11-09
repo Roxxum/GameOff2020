e5f3703669e830a84cd35c1370f5698208e9b694
Adam Pavey <adam@roxxum.tech>
2020-11-09 16:04:04 +1000

    better base code

--- game.py
+++ game.py
@@ -1,25 +1,34 @@
-import sys, pygame
-pygame.init()
-
-size = width, height = 1520, 780
-speed = [2, 2]
-black = 0, 0, 0
-
-screen = pygame.display.set_mode(size)
-
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
-
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+# import the pygame module, so you can use it
+import pygame
+ 
+# define a main function
+def main():
+     
+    # initialize the pygame module
+    pygame.init()
+    # load and set the logo
+    logo = pygame.image.load("logo32x32.png")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("minimal program")
+     
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode((240,180))
+     
+    # define a variable to control the main loop
+    running = True
+     
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+     
+     
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
+    main()
