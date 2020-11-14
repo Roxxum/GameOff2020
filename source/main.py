2cee660f41d665c73a1db62ab05b9740af5f06b0
Adam Pavey <adam@roxxum.tech>
2020-11-14 10:57:43 +1000

    code clean up

--- source/main.py
+++ source/main.py
@@ -31,42 +31,42 @@ def main():
     screen = pygame.display.set_mode((display_width,display_height))
     grass_width = 800
     grass_height = 20
-    grass_start_x = (display_width - grass_width)
-    grass_start_y = (display_height - grass_height)
+    grass_current_x = (display_width - grass_width)
+    grass_current_y = (display_height - grass_height)
     cannon_width = 60
     cannon_height = 100
-    cannon_start_x = (display_width * 0.45)
-    cannon_start_y = (display_height - (cannon_height + (grass_height / 2)))
-    cannon_x = 0
-    cannon_y = 0
+    cannon_current_x = (display_width * 0.45)
+    cannon_current_y = (display_height - (cannon_height + (grass_height / 2)))
+    cannon_change_x = 0
+    cannon_change_y = 0
     moon_width = 200
     moon_height = 200
-    moon_start_x = (moon_width * 0.0)
-    moon_start_y = (moon_height * 0.0)
-    moon_x = 0
-    moon_y = 0
+    moon_current_x = (moon_width * 0.0)
+    moon_current_y = (moon_height * 0.0)
+    moon_change_x = 0
+    moon_change_y = 0
     mice_width = 50
     mice_height = 60
-    mice_start_x = (0 - mice_width)
-    mice_start_y = (0 - mice_height)
-    mice_x = 0
-    mice_y = 0
+    mice_current_x = (0 - mice_width)
+    mice_current_y = (0 - mice_height)
+    mice_change_x = 0
+    mice_change_y = 0
      
     # define a variable to control the main loop
     running = True
 
     # define functions to control the assets
-    def cannon(cannon_start_x,cannon_start_y):
-        screen.blit(cannonIMG, (cannon_start_x, cannon_start_y))
+    def cannon(cannon_current_x,cannon_current_y):
+        screen.blit(cannonIMG, (cannon_current_x, cannon_current_y))
 
-    def moon(moon_start_x,moon_start_y):
-        screen.blit(moonIMG, (moon_start_x, moon_start_y))
+    def moon(moon_current_x,moon_current_y):
+        screen.blit(moonIMG, (moon_current_x, moon_current_y))
     
-    def mice(mice_start_x,mice_start_y):
-        screen.blit(miceIMG, (mice_start_x, mice_start_y))
+    def mice(mice_current_x,mice_current_y):
+        screen.blit(miceIMG, (mice_current_x, mice_current_y))
 
-    def grass(grass_start_x,grass_start_y):
-        screen.blit(grassIMG, (grass_start_x, grass_start_y))
+    def grass(grass_current_x,grass_current_y):
+        screen.blit(grassIMG, (grass_current_x, grass_current_y))
      
     # main loop
     while running:
@@ -76,55 +76,55 @@ def main():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
-            # add statements to move cannon
+            # add statements to move cannon with key press
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    if cannon_start_x > 5:
-                        cannon_x = -5
-                    if cannon_start_x < 5:
+                    if cannon_current_x > 5:
+                        cannon_change_x = -5
+                    if cannon_current_x < 5:
                         break
                 elif event.key == pygame.K_RIGHT:
-                    if cannon_start_x < (display_width - cannon_width):
-                        cannon_x = 5
-                    if cannon_start_x > (display_width - cannon_width):
+                    if cannon_current_x < (display_width - cannon_width):
+                        cannon_change_x = 5
+                    if cannon_current_x > (display_width - cannon_width):
                         break
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    cannon_x = 0
+                    cannon_change_x = 0
 
             # add statements to shoot mice
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    if mice_start_y <= (0 - mice_height):
-                        mice_start_y = (cannon_start_y + (mice_height / 2))
-                        mice_start_x = (cannon_start_x + ((cannon_width - mice_width) / 2))
-                    if mice_start_y > 5:
+                    if mice_current_y <= (0 - mice_height):
+                        mice_current_y = (cannon_current_y + ((cannon_height - mice_height) / 2))
+                        mice_current_x = (cannon_current_x + ((cannon_width - mice_width) / 2))
+                    if mice_current_y > 5:
                         break
 
         # add statements to change position of moon
-        if moon_start_x < display_width:
-            moon_x = 5
-        if moon_start_x > display_width:
-            moon_x = 0
-            moon_start_x = (0 - moon_width)
+        if moon_current_x < display_width:
+            moon_change_x = 5
+        if moon_current_x > display_width:
+            moon_change_x = 0
+            moon_current_x = (0 - moon_width)
 
         # add statements to change position of mice
-        if mice_start_y > (0 - mice_height):
-            mice_y = -5
-        if mice_start_y <= (0 - mice_height):
-            mice_y = 0
+        if mice_current_y > (0 - mice_height):
+            mice_change_y = -5
+        if mice_current_y <= (0 - mice_height):
+            mice_change_y = 0
 
         # change asset locations
-        cannon_start_x += cannon_x
-        mice_start_y += mice_y
-        moon_start_x += moon_x
+        cannon_current_x += cannon_change_x
+        mice_current_y += mice_change_y
+        moon_current_x += moon_change_x
         # set background colour
         screen.fill(grey)
         # display assets
-        grass(grass_start_x, grass_start_y)
-        mice(mice_start_x, mice_start_y)
-        cannon(cannon_start_x, cannon_start_y)
-        moon(moon_start_x, moon_start_y)
+        grass(grass_current_x, grass_current_y)
+        mice(mice_current_x, mice_current_y)
+        cannon(cannon_current_x, cannon_current_y)
+        moon(moon_current_x, moon_current_y)
         # load new frames each tick
         pygame.display.update()
         # set the frame rate 
